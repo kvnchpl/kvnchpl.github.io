d9cbb3e5a0c9e4baf23cd81ea66bc979a58b2547
kvnchpl <kvnchpl@gmail.com>
2025-05-19 12:39:33 -0400

    update

--- js/main.js
+++ js/main.js
@@ -1,6 +1,25 @@
 import { generateGalleryImages, loadJSON } from './utils.js';
 
 (async function () {
+    async function injectPartials() {
+        try {
+            const [headPartial, footerPartial] = await Promise.all([
+                fetch('/partials/head.html').then(res => res.text()),
+                fetch('/partials/footer.html').then(res => res.text())
+            ]);
+
+            document.head.insertAdjacentHTML('beforeend', headPartial);
+
+            const footerContainer = document.createElement('div');
+            footerContainer.innerHTML = footerPartial;
+            document.body.appendChild(footerContainer);
+        } catch (err) {
+            console.error("Error injecting partials:", err);
+        }
+    }
+
+    await injectPartials();
+
     const page = document.body.dataset.page || "index";
 
     try {
