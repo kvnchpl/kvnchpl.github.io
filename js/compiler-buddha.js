1e4a871b8c69ae5fa6b5c8278dcd9cdc6bac5d79
kvnchpl <kvnchpl@gmail.com>
2025-05-23 11:57:44 -0400

    update

--- js/compiler-buddha.js
+++ js/compiler-buddha.js
@@ -1,4 +1,4 @@
-function manifest(vision, threshold = 130, attention = 5, breadth = 20, expansion = 2) {
+function manifest(vision, threshold = 130, attention = 5, breadth = 20, expansion = 5/3) {
     const intention = document.getElementById("portal").textContent.replace(/\s+/g, "");
     const portal = document.createElement("canvas");
     const channel = portal.getContext("2d");
