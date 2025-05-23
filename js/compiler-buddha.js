6d85124b858013fb681370c0ebbc75a649b8b3cc
kvnchpl <kvnchpl@gmail.com>
2025-05-23 11:23:35 -0400

    update

--- js/compiler-buddha.js
+++ js/compiler-buddha.js
@@ -1,4 +1,4 @@
-function revisualize(vision, threshold = 128, attention = 5, breadth = 20, expansion = 1.546) {
+function revisualize(vision, threshold = 130, attention = 5, breadth = 20, expansion = 1.546) {
     const intention = document.getElementById("portal").textContent.replace(/\s+/g, "");
     const portal = document.createElement("canvas");
     const channel = portal.getContext("2d");
