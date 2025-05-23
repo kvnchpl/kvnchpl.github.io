6bdcfdab0bf9dd5b7c47faa378b0de3f22a552cd
kvnchpl <kvnchpl@gmail.com>
2025-05-23 14:56:18 -0400

    update

--- js/main.js
+++ js/main.js
@@ -73,13 +73,8 @@ import {
             renderNav(elementIds.nav, navData);
         }
 
-        // Early return for collection/index/404 pages after globals are set
-        if (isCollectionPage || page === "404" || page === "index") return;
-
-        if (page === "home") {
-            renderHomeLinks(navData, document.getElementById(elementIds.homeLinks));
-            return;
-        }
+        // Early return for collection/index/home/404 pages after globals are set
+        if (isCollectionPage || page === "index" || page === "home" || page === "404") return;
 
         // Render project or content page
         const pageContent = pages[page];
