37d310d4bcae9714b0c52c42a0366193dd2d26e6
kvnchpl <kvnchpl@gmail.com>
2025-05-26 14:48:49 -0400

    update

--- js/collections.js
+++ js/collections.js
@@ -4,41 +4,38 @@ async function waitForSiteConfig() {
         await new Promise(r => setTimeout(r, 10));
     }
 }
+
 (async function () {
     await waitForSiteConfig();
     const page = document.body.dataset.page;
     const siteConfig = window.siteConfig;
     const pages = window.pages;
-
     const collection = siteConfig.collections[page];
     if (!collection) return;
 
     const { type, basePath } = collection;
+    const container = document.getElementById(siteConfig.elementIds.collectionList);
+    if (!container || !type) return;
 
-    const listSection = document.getElementById(siteConfig.elementIds.collectionList);
-    if (listSection && type) {
-        const ul = document.createElement("ul");
-        Object.entries(pages).forEach(([slug, data]) => {
-            if (data.type === type) {
-                const pageLink = document.createElement("div");
-                pageLink.className = "page-link";
+    Object.entries(pages).forEach(([slug, data]) => {
+        if (data.type === type) {
+            const pageLink = document.createElement("div");
+            pageLink.className = "page-link";
 
-                const img = document.createElement("img");
-                img.src = data.thumbnail || "/img/index/sky-1.jpg";
-                img.alt = data.title || slug;
-                pageLink.appendChild(img);
+            const img = document.createElement("img");
+            img.src = data.thumbnail || "/img/index/sky-default.jpg";
+            img.alt = data.title || slug;
+            pageLink.appendChild(img);
 
-                const a = document.createElement("a");
-                a.href = `${basePath}${slug}.html`;
+            const a = document.createElement("a");
+            a.href = `${basePath}${slug}.html`;
 
-                const p = document.createElement("p");
-                p.textContent = data.title || slug;
-                a.appendChild(p);
+            const p = document.createElement("p");
+            p.textContent = data.title || slug;
+            a.appendChild(p);
 
-                pageLink.appendChild(a);
-                listSection.appendChild(pageLink);
-            }
-        });
-        listSection.appendChild(ul);
-    }
+            pageLink.appendChild(a);
+            container.appendChild(pageLink);
+        }
+    });
 })();
