e434bc0f3a57274a893de0cbaba602855ac15050
kvnchpl <kvnchpl@gmail.com>
2025-06-12 17:48:15 -0400

    update

--- tools/resize-thumbnail.js
+++ tools/resize-thumbnail.js
@@ -1,11 +1,24 @@
 const sharp = require('sharp');
 const path = require('path');
+const fs = require('fs');
 
-const inputPath = path.join(__dirname, 'img/projects/archipelago/thumbnail/archipelago_thumbnail.webp');
-const outputPath = inputPath;
+const inputArg = process.argv[2];
+if (!inputArg) {
+    console.error('Usage: node resize-thumbnail.js <path-to-image>');
+    process.exit(1);
+}
+
+const inputPath = path.resolve(inputArg);
+if (!fs.existsSync(inputPath)) {
+    console.error(`File not found: ${inputPath}`);
+    process.exit(1);
+}
+
+const { dir, name, ext } = path.parse(inputPath);
+const outputPath = path.join(dir, `${name}_400${ext}`);
 
 sharp(inputPath)
-  .resize({ width: 400 })
-  .toFile(outputPath)
-  .then(() => console.log('Image resized successfully.'))
-  .catch(err => console.error('Error resizing image:', err));
+    .resize({ width: 400 })
+    .toFile(outputPath)
+    .then(() => console.log(`Image resized to 400px width: ${outputPath}`))
+    .catch(err => console.error('Error resizing image:', err));
